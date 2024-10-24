feaa36b01aa2e455c89f441bb04b2577f5ac5f87
DawidB36 <147150149+DawidB36@users.noreply.github.com>
2024-10-24 23:41:33 +0200

    Update zestaw_nr4.java

--- zestaw_nr4.java
+++ zestaw_nr4.java
@@ -33,18 +33,18 @@ public class zestaw_nr4 {
     ///zad 2
     public static void wypiszTablice(int [] tab, int n, int m) {
         int k = 0;
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    if(k >= tab.length) {
-                        System.out.print("NaN ");
-                    }
-                    else {
-                        System.out.print(tab[k] + " ");
-                    }
-                    k++;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(k >= tab.length) {
+                    System.out.print("NaN ");
                 }
-                System.out.println("");
+                else {
+                    System.out.print(tab[k] + " ");
+                }
+                k++;
             }
+            System.out.println("");
+        }
     }
 
     ///zad 3
@@ -212,13 +212,129 @@ public class zestaw_nr4 {
     }
 
     public static double sredniaGeometryczna(int[] tab) {
-        int total = 1;
+        double total = 1;
 
         for (int i = 0; i < tab.length; i++) {
             total *= tab[i];
         }
 
-        return (double) Math.pow(total, 1/tab.length);
+        return Math.pow(total, 1.0/tab.length);
+    }
+
+    public static double sredniaHarmoniczna(int[] tab) {
+        double total = 0;
+
+        for (int i = 0; i < tab.length; i++) {
+            total += 1.0/tab[i];
+        }
+
+        return tab.length/total;
+    }
+
+    ///zad 5
+    public static int[] funkcjaLiniowa(int[] tab, int a, int b) {
+        int[] output_tab = new int[tab.length];
+
+        for(int i = 0; i < tab.length; i++) {
+            output_tab[i] = a * tab[i] + b;
+        }
+
+        return output_tab;
+    }
+
+    public static int[] funkcjaKwadratowa(int[] tab, int a, int b, int c) {
+        int[] output_tab = new int[tab.length];
+
+        for(int i = 0; i < tab.length; i++) {
+            output_tab[i] = a * (tab[i] * tab[i]) + b * tab[i] + c;
+        }
+
+        return output_tab;
+    }
+
+    public static double[] funkcjaWykladnicza(int[] tab, int a) {
+        double[] output_tab = new double[tab.length];
+
+        for(int i = 0; i < tab.length; i++) {
+            output_tab[i] = Math.pow(a, tab[i]);
+        }
+
+        return output_tab;
+    }
+
+    public static int[] funkcjaSignum(int[] tab) {
+        int[] output_tab = new int[tab.length];
+
+        for(int i = 0; i < tab.length; i++) {
+            if (tab[i] > 0)
+                output_tab[i] = 1;
+            else if (tab[i] < 0)
+                output_tab[i] = -1;
+            else
+                output_tab[i] = 0;
+        }
+
+        return output_tab;
+    }
+
+    ///zad 6
+    public static int najdluzszyCiagDodatnich(int[] tab) {
+        int maxLength = 0, currentLength = 0;
+
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i] > 0) {
+                currentLength++;
+            }
+            else {
+                maxLength = Math.max(maxLength, currentLength);
+                currentLength = 0;
+            }
+        }
+        maxLength = Math.max(maxLength, currentLength);
+
+        return maxLength;
+    }
+
+    public static int najdluzszyCiagUjemnych(int[] tab) {
+        int maxLength = 0, currentLength = 0;
+
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i] < 0) {
+                currentLength++;
+            } else {
+                maxLength = Math.max(maxLength, currentLength);
+                currentLength = 0;
+            }
+        }
+        maxLength = Math.max(maxLength, currentLength);
+
+        return maxLength;
+    }
+
+    public static void odwrocTablice(int[] tab) {
+        int left = 0;
+        int right = tab.length - 1;
+
+        while (left < right) {
+            int temp = tab[left];
+            tab[left] = tab[right];
+            tab[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    public static void odwrocTablice(int[] tab, int indeksStart, int indeksStop) {
+        int left = indeksStart;
+        int right = indeksStop;
+
+        while (left < right) {
+            int temp = tab[left];
+            tab[left] = tab[right];
+            tab[right] = temp;
+            left++;
+            right--;
+        }
     }
 
     ///zad 7
@@ -284,7 +400,8 @@ public class zestaw_nr4 {
 
         ///zad 4
         System.out.println("Zadanie nr 4:");
-        int[] tab4 = generujTablice(4, -10, 10);
+
+        int[] tab4 = generujTablice(4, 1, 10);
         drukujTabliceInt(tab4);
         System.out.println("\n");
 
@@ -297,6 +414,59 @@ public class zestaw_nr4 {
         System.out.println("Średnia arytmetyczna: " + sredniaArytmetyczna(tab4));
 
         System.out.println("Średnia geometryczna: " + sredniaGeometryczna(tab4));
+
+        System.out.println("Średnia harmoniczna: " + sredniaHarmoniczna(tab4));
+
+        System.out.println("\n");
+
+        ///zad 5
+        System.out.println("Zadanie nr 5:");
+
+        int[] tab5 = generujTablice(4, -10, 10);
+        drukujTabliceInt(tab5);
+        System.out.println("\n");
+
+        System.out.println("Nowa tablica z użyciem funkcji liniowej:");
+        int[] tab5_lin = funkcjaLiniowa(tab5, 2, 10);
+        drukujTabliceInt(tab5_lin);
+        System.out.println("\n");
+
+        System.out.println("Nowa tablica z użyciem funkcji kwadratowej:");
+        int[] tab5_kwad = funkcjaKwadratowa(tab5, 2, 10, -1);
+        drukujTabliceInt(tab5_kwad);
+        System.out.println("\n");
+
+        System.out.println("Nowa tablica z użyciem funkcji wykładniczej:");
+        double[] tab5_wyk = funkcjaWykladnicza(tab5, 2);
+        drukujTabliceDouble(tab5_wyk);
+        System.out.println("\n");
+
+        System.out.println("Nowa tablica z użyciem funkcji signum:");
+        int[] tab5_sig = funkcjaSignum(tab5);
+        drukujTabliceInt(tab5_sig);
+        System.out.println("\n");
+
+        ///zad 6
+        System.out.println("Zadanie nr 6:");
+
+        int[] tab6 = generujTablice(10, -10, 10);
+        drukujTabliceInt(tab6);
+        System.out.println("\n");
+
+        System.out.println("Najdłuższy ciąg liczb dodatnich: " + najdluzszyCiagDodatnich(tab6));
+        System.out.println("\n");
+
+        System.out.println("Najdłuższy ciąg liczb ujemnych: " + najdluzszyCiagUjemnych(tab6));
+        System.out.println("\n");
+
+        odwrocTablice(tab6);
+        System.out.println("Tablica po odwróceniu:");
+        drukujTabliceInt(tab6);
+        System.out.println("\n");
+
+        odwrocTablice(tab6, 2, 7);
+        System.out.println("Tablica po odwróceniu od indeksu 2 do 7:");
+        drukujTabliceInt(tab6);
 
         System.out.println("\n");
 
